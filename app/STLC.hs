9796f9a5ea5cd40244153d220a33ca35433bdb7c
Eugene Akentyev <ak3ntev@gmail.com>
2017-06-18 19:20:05 +0500

    Add ugly type checking, nats, and evaluation step of if.

--- app/STLC.hs
+++ app/STLC.hs
@@ -14,16 +14,20 @@ import Abt.Class
 import Abt.Types
 import Abt.Concrete.LocallyNameless
 
+import Prelude hiding (succ)
 import Util
 
 data Lang ns where
-  LAM :: Lang '[S Z]
+  LAM :: Lang '[Z, S Z]
   APP :: Lang '[Z, Z]
   TRUE :: Lang '[]
   FALSE :: Lang '[]
   IF :: Lang '[Z, Z, Z]
   BOOL :: Lang '[]
   ARROW :: Lang '[Z, Z]
+  NAT :: Lang '[]
+  ZERO :: Lang '[]
+  SUCC :: Lang '[Z]
 
 instance Show1 Lang where
   show1 = \case
@@ -34,6 +38,9 @@ instance Show1 Lang where
     IF -> "if"
     BOOL -> "bool"
     ARROW -> "->"
+    NAT -> "nat"
+    ZERO -> "zero"
+    SUCC -> "succ"
 
 instance HEq1 Lang where
   heq1 LAM LAM = Just Refl
@@ -43,10 +50,13 @@ instance HEq1 Lang where
   heq1 IF IF = Just Refl
   heq1 BOOL BOOL = Just Refl
   heq1 ARROW ARROW = Just Refl
+  heq1 NAT NAT = Just Refl
+  heq1 ZERO ZERO = Just Refl
+  heq1 SUCC SUCC = Just Refl  
   heq1 _ _ = Nothing
 
-lam :: Tm Lang (S Z) -> Tm0 Lang
-lam e = LAM $$ e :& RNil
+lam :: Tm0 Lang -> Tm Lang (S Z) -> Tm0 Lang
+lam t e = LAM $$ t :& e :& RNil
 
 app :: Tm0 Lang -> Tm0 Lang -> Tm0 Lang
 app m n = APP $$ m :& n :& RNil
@@ -57,14 +67,23 @@ true = TRUE $$ RNil
 false :: Tm0 Lang
 false = FALSE $$ RNil
 
-if_then_else :: Tm0 Lang -> Tm0 Lang -> Tm0 Lang -> Tm0 Lang
-if_then_else c t1 t2 = IF $$ c :& t1 :& t2 :& RNil
+if_ :: Tm0 Lang -> Tm0 Lang -> Tm0 Lang -> Tm0 Lang
+if_ c t1 t2 = IF $$ c :& t1 :& t2 :& RNil
 
 bool :: Tm0 Lang
 bool = BOOL $$ RNil
 
 arrow :: Tm0 Lang -> Tm0 Lang -> Tm0 Lang
 arrow a b = ARROW $$ a :& b :& RNil
+
+nat :: Tm0 Lang
+nat = NAT $$ RNil
+
+zero :: Tm0 Lang
+zero = ZERO $$ RNil
+
+succ :: Tm0 Lang -> Tm0 Lang
+succ t = SUCC $$ t :& RNil
 
 step
   :: Tm0 Lang
@@ -73,8 +92,12 @@ step tm =
   out tm >>= \case
     APP :$ m :& n :& RNil ->
       out m >>= \case
-        LAM :$ xe :& RNil -> xe // n
+        LAM :$ t :& xe :& RNil -> xe // n
         _ -> app <$> step m <*> pure n <|> app <$> pure m <*> step n
+    IF :$ c :& t1 :& t2 :& RNil ->
+      out c >>= \case
+        TRUE :$ RNil -> return t1
+        FALSE :$ RNil -> return t2
     _ -> stepsExhausted
 
 star
@@ -93,52 +116,99 @@ checkTy
   -> Tm0 Lang
   -> JudgeT M ()
 checkTy g tm ty = do
-  let ntm = eval tm
-      nty = eval ty
-  (,) <$> out ntm <*> out nty >>= \case
-    -- (LAM :$ xe :& RNil, ARROW :$ a :& y :& RNil) -> do
-    --   z <- fresh
-    --   ez <- xe // var z
-    --   bz <- y // var z
-    --   checkTy ((z,a):g) ez bz
-    (IF :$ c :& t1 :& t2 :& RNil, _) -> do
-      cty <- inferTy g c
-      t1ty <- inferTy g t1
-      t2ty <- inferTy g t2
-      case (cty === bool, t1ty === t2ty && t1ty === ty) of
-        (True, True) -> return ()
-        (False, _) -> raise "The condition must have type bool"
-        (_, False) -> raise "Terms types mismatch"
-    _ -> do
-      ty' <- inferTy g tm
-      if ty' === nty
-        then return ()
-        else raise "Type error"
+  ty' <- inferTy g tm
+  if ty' === ty
+    then return ()
+    else raise "Type error"
 
 inferTy
   :: Ctx
   -> Tm0 Lang
   -> JudgeT M (Tm0 Lang)
 inferTy g tm = do
-  out (eval tm) >>= \case
+  out tm >>= \case
     V v | Just (eval -> ty) <- lookup v g -> return ty
         | otherwise -> raise "Ill-scoped variable"
-    APP :$ m :& n :& RNil -> do
-      mTy <- inferTy g m
-      nTy <- inferTy g n
-      return $ arrow mTy nTy
+    LAM :$ t :& m :& RNil -> do
+        z <- fresh
+        em <- m // var z
+        ty <- inferTy ((z,t):g) em
+        return $ arrow t ty
+    APP :$ t1 :& t2 :& RNil -> do
+      t1Ty <- inferTy g t1
+      t2Ty <- inferTy g t2
+      out t1Ty >>= \case
+        ARROW :$ t1Ty1 :& t1Ty2 :& RNil ->
+          if t1Ty1 === t2Ty
+            then return t1Ty2
+            else raise "Parameter type mismatch"
+        _ -> raise "Arrow type expected"
+    IF :$ c :& t1 :& t2 :& RNil -> do
+      cty <- inferTy g c
+      t1ty <- inferTy g t1
+      t2ty <- inferTy g t2
+      case (cty === bool, t1ty === t2ty) of
+        (True, True) -> return t1ty
+        (False, _) -> raise "The condition must have type bool"
+        (_, False) -> raise "Terms types mismatch"
     TRUE :$ RNil -> return bool
     FALSE :$ RNil -> return bool
+    ZERO :$ RNil -> return nat
+    SUCC :$ t :& RNil -> do
+      ty <- inferTy g t
+      if ty === nat
+        then return nat
+        else raise "The argument is not a number"
     _ -> raise "Failure"
 
 eval :: Tm0 Lang -> Tm0 Lang
 eval = runM . star step
 
+judge :: JudgeT M String -> IO ()
+judge = either fail putStrLn . runM . runExceptT . runJudgeT
+
 main :: IO ()
 main = do
-  either fail print . runM . runExceptT . runJudgeT $ do
+  judge $ do
     ty <- inferTy [] false
     tyS <- toString ty
     return tyS
 
-  print "Hello"
+  judge $ do
+    x <- named "x"
+    ty <- inferTy [] (lam bool (x \\ var x))
+    tyS <- toString ty
+    return tyS
+
+  judge $ do
+    x <- named "x"
+    checkTy [] (lam bool (x \\ var x)) (arrow bool bool)
+    return "Success"
+
+  judge $ do
+    checkTy [] false bool
+    return "Success"
+
+  judge $ do
+    x <- named "x"
+    let tm = (app (lam bool (x \\ var x)) true)
+    tmT <- inferTy [] tm
+    checkTy [] tm bool
+    return "Success"
+
+  judge $ do
+    tmT <- inferTy [] (succ zero)
+    tmS <- toString tmT
+    return tmS
+
+  print . runM $ do
+    x <- named "x"
+    let tm = (app (lam bool (x \\ false)) true)
+    tmS <- toString $ eval tm
+    return tmS
+
+  print . runM $ do
+    x <- named "x"
+    let tm = if_ true false true
+    tmS <- toString $ eval tm
+    return tmS
